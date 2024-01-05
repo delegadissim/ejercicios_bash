6e54f7bf54d6bb239f0f884bad977df3a4637735
Delegadissim <alumas9513@ieselcaminas.org>
2024-01-05 18:19:28 +0100

    arreglats els errors

--- ex/ex14.sh
+++ ex/ex14.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-ls -la $1 | awk '{print substr($1, 1, 1), $NF}' | tail -n +4
+if [ ! -e "$1" ]; then
+    echo "La ruta no existe."
+    exit
+fi
 
+
+
+ls -la $1 | awk '{print substr($1, 1, 1), $NF}' | tail -n +4
+ls -la $1 | awk '{print substr($1, 1, 1), $NF}' | tail -n +4 | wc -l 
